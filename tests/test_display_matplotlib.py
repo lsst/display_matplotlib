8abbd25498dd52f7602cb6afae6314ebf2572f93
Robert Lupton the Good <rhl@astro.princeton.edu>
2017-11-21 15:13:00 -0500

    Added a test that we can set the afwDisplay backend to "matplotlib"
    N.b. this will cause the package to be imported, so that's checked too

---
+++ tests/test_display_matplotlib.py
@@ -0,0 +1,51 @@
+#
+# LSST Data Management System
+# Copyright 2017 LSST Corporation.
+#
+# This product includes software developed by the
+# LSST Project (http://www.lsst.org/).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the LSST License Statement and
+# the GNU General Public License along with this program.  If not,
+# see <http://www.lsstcorp.org/LegalNotices/>.
+#
+"""
+Tests for display_matplotlib
+"""
+import unittest
+
+import lsst.afw.display as afwDisplay
+
+class DisplayMatplotlibTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testSetAfwDisplayBackend(self):
+        """Set the default backend to this package"""
+        afwDisplay.setDefaultBackend("matplotlib")
+
+class TestMemory(lsst.utils.tests.MemoryTestCase):
+    pass
+
+
+def setup_module(module):
+    lsst.utils.tests.init()
+
+
+if __name__ == "__main__":
+    lsst.utils.tests.init()
+    unittest.main()
