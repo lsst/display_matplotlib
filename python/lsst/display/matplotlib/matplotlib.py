358b4d7841f9c02c84ef1b7a676770451d913b47
Robert Lupton the Good <rhl@astro.princeton.edu>
2018-07-27 14:51:32 -0500

    Changed the argument to setImageColormap to be a string or Colormap

--- python/lsst/display/matplotlib/matplotlib.py
+++ python/lsst/display/matplotlib/matplotlib.py
@@ -151,12 +151,25 @@ class DisplayImpl(virtualDevice.DisplayImpl):
                 self._figure.colorbar(self._mappable)
 
     def _setImageColormap(self, cmap):
-        """Set the colormap used for th eimage"""
+        """Set the colormap used for the image
+
+        cmap should be either the name of an attribute of pyplot.cm or an mpColors.Colormap
+        (e.g. "gray" or pyplot.cm.gray)
+
+        """
+        if not isinstance(cmap, mpColors.Colormap):
+            cmap = getattr(pyplot.cm, cmap)
+
         self._image_colormap = cmap
 
-    # This will eventually be moved into Display
+    # This will be moved into Display (DM-15218)
     def setImageColormap(self, cmap):
-        """Set the colormap used for th eimage"""
+        """Set the colormap used for the image
+
+        cmap is a string to be interpreted by the backend; where possible
+        a string such as "gray" will be honoured, but backend
+        specific values are also permitted
+        """
         self._setImageColormap(cmap)
 
     #
